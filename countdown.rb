5630f8c9d3af90dc841c45c7791cc30103816009
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-03 18:41:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
-
-def countdown
+counter =10
+def countdown(integer)
+  while integer>0
+    puts "#{integer} SECOND(S)!"
+    counter -= 1
+end
+puts "HAPPY NEW YEAR!"
+end
 end
